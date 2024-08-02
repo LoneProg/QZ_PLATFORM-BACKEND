caa4d2390c828fe626add32a32f11d9e1e04a72a
Dalaktronixs <hakeemabdullah87@gmail.com>
2024-08-02 18:19:32 +0100

    FEAT: create a boiler plate code to test server

--- server.js
+++ server.js
@@ -1,4 +1,17 @@
 const express = require("express");
-const dotenv = require("dotenv");
+const dotenv = require("dotenv").config();
 
-//
+
+//configuring server
+const server = express();
+const port = process.env.PORT
+
+//Basic route 
+server.get('/', (req, res) => {
+    res.send("Hello World");
+})
+
+//running Server
+server.listen(port, () =>  {
+    console.log(`Server running on Port ${port}`);
+});
