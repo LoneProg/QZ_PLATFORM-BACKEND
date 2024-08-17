e5f56028cfe17dd02d71d9e11919aeb169a6931b
Dalaktronixs <hakeemabdullah87@gmail.com>
2024-08-17 17:10:54 +0100

    FEAT: add the question model file models/questions.js

---
+++ models/questions.js
@@ -0,0 +1,17 @@
+const mongoose = require ("mongoose");
+const Schema = mongoose.Schema;
+
+
+const questionSchema = new Schema({
+    type: { type: String, enum:["multiChoice, TrueFalse, shortAnswer"], required: true},
+    text: { type: String, required: true},
+    options: [String],
+    correctAnswer: { type: Schema.Types.Mixed},
+    settings: {
+        points: { type: Number},
+        category: { type: String},
+        randomizeOptions: { type: Boolean}
+    }
+},{timestamps: true});
+
+modules.export = mongoose.model("Question", questionSchema)
--- models/tests.js
+++ models/tests.js
@@ -3,14 +3,17 @@ const Schema = mongoose.Schema;
 const TestSchema = new Schema({
     title: { type: String, required: true },
     description: { type: String },
+    category: { type: String },
     createdBy: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+    instruction: { type: String },
     settings: {
         startDate: { type: Date },
         endDate: { type: Date },
         timeLimit: { type: Number },
         attempts: { type: Number },
         availability: { type: String, enum: ['Open', 'Restricted'], default:'Restricted' },
-        price: { type: Number },
+        randomize: { type: Boolean },
+        passingScore: { type: Number},
         accessCode: { type: String }
     }
 }, {
