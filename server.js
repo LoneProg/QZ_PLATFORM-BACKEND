cc93348924c4a5cc369984dd07e6e45b75e875e7
Dalaktronixs <hakeemabdullah87@gmail.com>
2024-08-17 12:12:17 +0100

    FEAT: create the test controller, routes files

--- server.js
+++ server.js
@@ -15,6 +15,7 @@ app.use(express.json());
 app.use("/api/users", require("./routes/userRoutes")); 
 app.use("/api/groups", require("./routes/groupRoutes")); 
 app.use("/api/auths", require("./routes/authRoutes"));
+app.use("/api/tests", require("./routes/testRoutes"));
 app.use(errorHandler);
 
 // Connect to MongoDB
