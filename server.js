cba2de0ccf6a3d99e0aa0f35b0fb25d76b8c4b5c
Jummy007 <gbemisoyeadejumoke@gmail.com>
2024-08-26 12:51:25 +0100

    updating merge

--- server.js
+++ server.js
@@ -1,9 +1,7 @@
 const express = require("express");
 const errorHandler = require("./middlewares/errorHandler");
-const connectDB = require("./config/db");
-require("dotenv").config();
-const authRoutes = require("./routes/authRoutes");
-const testRoutes = require("./routes/testRoutes");
+const connectDB = require('./config/db');
+require('dotenv').config();
 
 // configuring server
 const app = express();
@@ -13,7 +11,6 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use("/api/users", require("./routes/userRoutes")); 
 app.use("/api/groups", require("./routes/groupRoutes")); 
-app.use("/api/tests", testRoutes);
 
 app.use(errorHandler);
 
@@ -22,5 +19,5 @@ connectDB();
 
 // Running Server
 app.listen(port, () => {
-  console.log(`Server running on ${port}`);
+    console.log(`Server running on ${port}`);
 });
