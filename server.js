e2355412e9a3dbdcb9822b958a737e8da729759b
Dalaktronixs <hakeemabdullah87@gmail.com>
2024-09-12 12:56:17 +0100

    UPDATE: update th server.js to include PATCH method in the CORS Middleware

--- server.js
+++ server.js
@@ -18,7 +18,7 @@ const port = process.env.PORT || 3000;
 // Enable CORS for your frontend (localhost:5173) before other middleware
 app.use(cors({
     origin: 'http://localhost:5173', // Allow requests from this origin
-    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Specify allowed methods
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Specify allowed methods
     credentials: true, // Enable this if requests involve cookies or sessions
 }));
 
