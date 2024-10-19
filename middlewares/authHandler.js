ec5231573afbe833953d6df42d19e3a6f9b0bf03
Dalaktronixs <hakeemabdullah87@gmail.com>
2024-10-19 12:18:10 +0100

    BUGFIX: fix access token issue in the authHandler.js

--- middlewares/authHandler.js
+++ middlewares/authHandler.js
@@ -7,7 +7,7 @@ const authenticateToken = (req, res, next) => {
 
   if (token == null) return res.status(401).json({ message: 'Token is required' });
 
-  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+  jwt.verify(token, process.env.JWT_SECRET, (err, user) => {
     if (err) return res.status(403).json({ message: 'Invalid token' });
     req.user = user;
     next();
